d1d4a36866d5aaf69e1ed60167bc052323f67040
Daniel Magues <dmagues@hotmail.com>
2014-04-10 15:00:54 -0500

    Add icon reference in R.java

--- gen/com/oreilly/nasadailyimage/R.java
+++ gen/com/oreilly/nasadailyimage/R.java
@@ -27,11 +27,12 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int ic_navigation_refresh=0x7f020001;
-        public static final int ic_share=0x7f020002;
-        public static final int ic_wallpaper=0x7f020003;
-        public static final int test_image=0x7f020004;
+        public static final int ic_action_read=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int ic_navigation_refresh=0x7f020002;
+        public static final int ic_share=0x7f020003;
+        public static final int ic_wallpaper=0x7f020004;
+        public static final int test_image=0x7f020005;
     }
     public static final class id {
         public static final int action_feed=0x7f090011;
@@ -69,6 +70,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
+        public static final int feed_button=0x7f06000c;
         public static final int refresh_button=0x7f060007;
         public static final int set_wallpaper_button=0x7f060008;
         public static final int share_button=0x7f06000b;
